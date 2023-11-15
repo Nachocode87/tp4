e4a8b646f0a91a4d7d3f4c25b98ebf3435c50d2e
nachocode <lic.medinajimenez@gmail.com>
2023-11-15 19:41:48 -0300

    tercer ej

---
+++ js/tp4ejercicio3.js
@@ -0,0 +1,30 @@
+class Rectangulo {
+    #alto;
+    #ancho;
+    constructor(altoP,anchoP) {
+        this.#alto = altoP;
+        this.#ancho = anchoP;
+    }
+    get alto () {
+        return this.#alto;
+    }
+    set alto (alto) {
+        this.#alto = alto;
+    }
+    get ancho () {
+        return this.#ancho;
+    }
+    set ancho (ancho) {
+        this.#ancho = ancho;
+    }
+
+    perimetro() {
+        return this.alto*2 + this.ancho*2;
+    }
+    area() {
+        return this.alto*this.ancho;
+    }
+    mostrarPropiedades() {
+        console.log(`el alto es ${this.alto} y el ancho ${this.ancho} del rectangulo, su perimetro es ${this.perimetro()} y su area ${this.area()}`)
+    }
+}
